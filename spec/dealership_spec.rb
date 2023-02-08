f33ca26d1467f64014811506773bf42bdf2b4422
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-08 10:40:10 -0700

    write first two tests

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/dealership'
+
+RSpec.describe Dealership do
+  it 'exists' do 
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership).to be_a(Dealership)
+  end
+
+  xit 'has an inventory' do 
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+  end
+

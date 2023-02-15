cc68bf0781bfdc269c31feea40b1dbff490b42c8
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-02-14 22:58:54 -0700

    define dealership class

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,10 +1,44 @@
 class Dealership
-  attr_reader :inventory, :inventory_count, :name, :address
+  attr_reader :inventory, :name, :address
 
-  def initialize(name, address, inventory = [], inventory_count = 0)
-    @name
-    @address
-    @inventory
-    @inventory_count
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
+
+  def add_car(car)
+    @inventory << car
+  end
+
+  def inventory_count
+    @inventory.length
+  end
+
+  def has_inventory?
+    if @inventory.length > 0
+      true
+    else
+      false
+    end
+  end
+
+  def total_value
+    @inventory.sum do |car|
+    car.total_cost
+    end
+  end
+
+  def cars_by_make(make)
+    @inventory.select do |car|
+      car.make == make 
+    end
+  end
+
+  def details
+    hash_deets = Hash.new
+    hash_deets["total_value"] = total_value
+    hash_deets["address"] = @address
+    hash_deets
   end
 end 

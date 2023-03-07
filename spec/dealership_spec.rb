07cee13a330eb25e7bddf019e08865a5e273a0fa
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 15:12:11 -0700

    refactor & add tests

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -5,8 +5,13 @@ require'./lib/car'
 RSpec.describe Dealership do
 before(:each) do 
   @dealership = Dealership.new("Acme Auto", "123 Main Street")
+  @car_1 = Car.new("Ford Mustang", 1500, 36)
+  @car_2 = Car.new("Toyota Prius", 1000, 48)
+  @car_3 = Car.new("Toyota Tercel", 500, 48)
+  @car_4 = Car.new("Chevrolet Bronco", 1250, 24)
 end
-  describe '#properties' do
+
+describe '#initialize' do
   it 'exists' do 
     expect(@dealership).to be_a(Dealership)
   end
@@ -15,23 +20,24 @@ end
     expect(@dealership.inventory).to eq([])
   end
 
-  it 'has an inventory' do
+  it 'has an inventory count' do
     expect(@dealership.inventory_count).to eq(0)
   end
 
-  it 'stores cars in @inventory' do
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    @dealership.add_car(car_1)
-    @dealership.add_car(car_2)
+  it 'has an address' do
+    expect(@dealership.address).to eq("123 Main Street")
+  end
+
+  it 'stores cars in inventory' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
     expect(@dealership.inventory).to be_a(Array)
+    expect(@dealership.inventory).to eq([@car_1, @car_2])
   end
 
   it 'counts how many cars are in its inventory' do
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    @dealership.add_car(car_1)
-    @dealership.add_car(car_2)
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
     expect(@dealership.inventory_count).to eq(2)
   end
 
@@ -40,54 +46,46 @@ end
   end
 
   it 'has_inventory? yes' do
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    car_3 = Car.new("Toyota Tercel", 500, 48)
-    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
-    @dealership.add_car(car_1)
-    @dealership.add_car(car_2)
-    @dealership.add_car(car_3)
-    @dealership.add_car(car_4)
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
     expect(@dealership.has_inventory?).to eq(true)
   end
 
   it 'returns cars by make' do
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    car_3 = Car.new("Toyota Tercel", 500, 48)
-    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
-    @dealership.add_car(car_1)
-    @dealership.add_car(car_2)
-    @dealership.add_car(car_3)
-    @dealership.add_car(car_4)
-    expect(@dealership.cars_by_make("Ford")).to eq([car_1])
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+    expect(@dealership.cars_by_make("Ford")).to eq([@car_1])
   end
 
   it 'determines how wealthy the dealership is' do
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    car_3 = Car.new("Toyota Tercel", 500, 48)
-    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
-    @dealership.add_car(car_1)
-    @dealership.add_car(car_2)
-    @dealership.add_car(car_3)
-    @dealership.add_car(car_4)
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
     expect(@dealership.total_value).to eq(156000)
   end
 
   it 'stores details in a hash' do
-    car_1 = Car.new("Ford Mustang", 1500, 36)
-    car_2 = Car.new("Toyota Prius", 1000, 48)
-    car_3 = Car.new("Toyota Tercel", 500, 48)
-    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
-    @dealership.add_car(car_1)
-    @dealership.add_car(car_2)
-    @dealership.add_car(car_3)
-    @dealership.add_car(car_4)
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
     expect(@dealership.details).to eq({
     "total_value" => 156000, "address" => "123 Main Street"
     })
-    end
+  end
+  
+  it 'finds average_price_of_car' do
+    @dealership.add_car(@car_1)
+    @dealership.add_car(@car_2)
+    @dealership.add_car(@car_3)
+    @dealership.add_car(@car_4)
+    expect(@dealership.average_price_of_car).to eq("39,000")
+  end
 end
 end
 

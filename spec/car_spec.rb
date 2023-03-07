0cac71c80f9a1fed7250157ee8ea56b2810f5983
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 14:58:55 -0700

    refactor tests

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,47 +2,35 @@ require 'rspec'
 require './lib/car'
 
 RSpec.describe Car do
+  before(:each) do 
+    @car = Car.new("Ford Mustang", 1500, 36)
+  end
   it 'exists' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car).to be_a Car
+    expect(@car).to be_a Car
   end
 
   it 'has a make' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.make).to eq("Ford")
+    expect(@car.make).to eq("Ford")
   end
 
   it 'has a model' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.model).to eq("Mustang")
+    expect(@car.model).to eq("Mustang")
   end
 
   it 'has a monthly payment' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.monthly_payment).to eq(1500)
+    expect(@car.monthly_payment).to eq(1500)
   end
 
   it 'has a loan length' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.loan_length).to eq(36)
+    expect(@car.loan_length).to eq(36)
   end
 
   it 'has a total cost' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.total_cost).to eq(54000)
+    expect(@car.total_cost).to eq(54000)
   end
 
   it 'has a color' do
-    car = Car.new("Ford Mustang", 1500, 36)
-    car.paint!(:blue)
-
-    expect(car.color).to eq(:blue) 
+   @car.paint!(:blue)
+   expect(@car.color).to eq(:blue) 
   end
-
 end
